b8cf479312d7c9d0cd6d5007c8ff4163646cf8d3
Harrison <35613146+hmwirtz@users.noreply.github.com>
2018-05-10 20:56:19 -0700

    clickr

--- app/src/main/java/e/harrison/dogtrain/MainActivity.java
+++ app/src/main/java/e/harrison/dogtrain/MainActivity.java
@@ -4,46 +4,99 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button goodDog, whistle;
-    TextView sitCounter;
+    Button goodDog, whistle, switchButton;
+    TextView sitCounter, trickSwitch;
+    String counterUpdate, counterInit;
+    int counterUpdated, counter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        switchButton = findViewById(R.id.trick_Change);
         goodDog = findViewById(R.id.goodDog_button);
         whistle = findViewById(R.id.whistle_button);
         sitCounter = findViewById(R.id.sitTracker_counter);
         goodDog.setOnClickListener(goodDogOnClickListener);
         whistle.setOnClickListener(whistleOnClickListener);
+        trickSwitch = findViewById(R.id.sitTracker);
+        switchButton.setOnClickListener(trickButton);
+
+
+
+        sitCounter.setText("0");
     }
 
-    public void sitTrackerUpdate() {
-        int counter = Integer.parseInt(sitCounter.getText().toString());
-        int counterUpdated = ++counter;
+    public void switchTrick(){
 
-        String counterUpdate = String.valueOf(counterUpdated);
+        String currentTrick = trickSwitch.getText().toString();
+        Intent intent = new Intent(MainActivity.this, TrainingLog.class);
+        intent.putExtra("Trick", currentTrick);
 
+        if (currentTrick.equals("Sit Tracker")){
+
+            trickSwitch.setText("Lay Tracker");
+            counter = 0;
+            sitCounter.setText("0");
+
+        }
+        else if (currentTrick.equals("Lay Tracker")){
+
+            trickSwitch.setText("Shake Tracker");
+            counter = 0;
+            sitCounter.setText("0");
+        }
+
+        else if (currentTrick.equals("Shake Tracker")){
+
+            trickSwitch.setText("Sit Tracker");
+            counter = 0;
+            sitCounter.setText("0");
+        }
+
+
+
+
+
+    }
+
+    View.OnClickListener trickButton = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            switchTrick();
+        }
+    };
+
+
+
+
+    public void TrackerUpdate() {
+
+
+        counterUpdated = ++counter;
+        counterUpdate = String.valueOf(counterUpdated);
         sitCounter.setText(counterUpdate);
 
-        Intent sendClickCount = new Intent();
-        sendClickCount.putExtra(counterUpdate,counterUpdated);
+
+
+
     }
+
 
     View.OnClickListener goodDogOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            sitTrackerUpdate();
+            TrackerUpdate();
 
         }
 
@@ -53,11 +106,27 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
 
+            counterInit = String.valueOf(counter);
+
+
+            Intent intent = new Intent(MainActivity.this, TrainingLog.class);
+
+            if (counter == 0) {
+                intent.putExtra("counted", counterInit);
+            } else {
+                intent.putExtra("counted", counterUpdate);
+            }
+            MainActivity.this.startActivity(intent);
+
+
         }
+
+
     };
+}
 
 
-    }
+
 
 
 
--- app/src/main/java/e/harrison/dogtrain/TrainingLog.java
+++ app/src/main/java/e/harrison/dogtrain/TrainingLog.java
@@ -2,21 +2,127 @@ package e.harrison.dogtrain;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TrainingLog extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private String trackerUpdate;
+    private TextView test;
+    private Intent intent;
+    private Button backButton, shakeButton, layButton, sitButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_log);
 
+        progressBar = findViewById(R.id.progressBar);
+        test = findViewById(R.id.test);
+        intent = getIntent();
+        backButton = findViewById(R.id.BACK_BUTTON);
+        backButton.setOnClickListener(backToMainListener);
+        shakeButton = findViewById(R.id.SHAKE_BUTTON);
+        layButton = findViewById(R.id.LAY_BUTTON);
+        sitButton = findViewById(R.id.SIT_BUTTON);
+
+        shakeButton.setOnClickListener(shakeListener);
+        layButton.setOnClickListener(layListener);
+        sitButton.setOnClickListener(sitListener);
+
+
+
+
+        setProgressBar();
+
 
 
     }
 
-}
+    View.OnClickListener backToMainListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent mainIntent = new Intent(TrainingLog.this,MainActivity.class);
+            TrainingLog.this.startActivity(mainIntent);
+        }
+    };
+
+    void setProgressBar() {
+
+
+        Bundle bundle = intent.getExtras();
+        trackerUpdate = bundle.getString("counted");
+        int counter = Integer.parseInt(trackerUpdate);
+        progressBar.setProgress(counter);
+        progressBar.setMax(50);
+        test.setText(trackerUpdate);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    View.OnClickListener sitListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            intent = new Intent(TrainingLog.this, MainActivity.class);
+            Bundle bundle = intent.getExtras();
+            String sitCounter = bundle.getString("Trick");
+            String sitCount = bundle.getString("counted");
+
+            if (sitCounter.equals("Sit Tracker")) {
+
+                int counter = Integer.parseInt(sitCount);
+
+                test.setText(sitCount);
+
+            }
+
+
+        }
+
+    };
+
+
+        View.OnClickListener shakeListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = intent.getExtras();
+                bundle.getString("currentTrick");
+
+
+            }
+        };
+
+        View.OnClickListener layListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = intent.getExtras();
+                bundle.getString("currentTrick");
+
+
+            }
+        };
+    }
+
+
+
+
+
